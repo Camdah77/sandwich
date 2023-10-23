19259ef7b580c3a968363d3563a1cfed7e3180ca
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2023-10-23 10:19:07 +0000

    Made sales and surplus functions as comments since they are replaced by refactoring

--- run.py
+++ run.py
@@ -53,7 +53,8 @@ def validate_data(values):
 
     return True
 
-
+"""
+THE UPDATE_WORKSHEET FUNCTION IS REPLACING THIS TWO FUNCTIONS
 def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided
@@ -72,6 +73,7 @@ def update_surplus_worksheet(data):
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated successfully.\n")
+"""
 
 def update_worksheet(data, worksheet):
      print(f"Updating {worksheet} worksheet...\n")
